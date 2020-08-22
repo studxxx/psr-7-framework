399555c59d5c667a0ec57ad9f4cf64ad3c48659c
studxxx <stud181177@gmail.com>
2020-08-22 13:01:06 +0300

    Added primitive routing

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
@@ -14,9 +15,15 @@ $request = ServerRequestFactory::fromGlobals();
 
 ### Action
 
-$name = $request->getQueryParams()['name'] ?? 'Guest';
+$path = $request->getUri()->getPath();
 
-$response = new HtmlResponse('Hello, ' . $name . '!');
+if ($path === '/') {
+    $response = new HtmlResponse('Hello, ' . $request->getQueryParams()['name'] ?? 'Guest' . '!');
+} elseif ($path === '/about') {
+    $response = new HtmlResponse('I am a simple site.');
+} else {
+    $response = new JsonResponse(['error' => 'Undefined page'], 404);
+}
 
 ### PostProcessing
 
