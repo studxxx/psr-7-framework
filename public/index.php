1daca27da02d89d5d0abd9b7e2452320add8bfac
studxxx <stud181177@gmail.com>
2020-08-22 11:10:45 +0300

    Updated index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 use Framework\Http\RequestFactory;
 use Framework\Http\Response;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\ServerRequestFactory;
 
 chdir(dirname(__DIR__));
 
@@ -9,19 +11,26 @@ require 'vendor/autoload.php';
 
 ### Initialization
 
-$request = RequestFactory::fromGlobals();
+$request = ServerRequestFactory::fromGlobals();
 
 ### Action
 
 $name = $request->getQueryParams()['name'] ?? 'Guest';
 
-$response = (new Response('Hello, ' . $name . '!'))
+$response = (new HtmlResponse('Hello, ' . $name . '!'))
     ->withHeader('X-Developer', 'studxxx');
 
 ### Sending
 
-header('HTTP/1.0' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+header(sprintf(
+    'HTTP/%s %d %s',
+    $response->getProtocolVersion(),
+    $response->getStatusCode(),
+    $response->getReasonPhrase()
+));
 foreach ($response->getHeaders() as $name => $values) {
-    header($name . ':' . implode(', ', $values));
+    foreach ($values as $value) {
+        header(sprintf('%s: %s', $name, $value), false);
+    }
 }
 echo $response->getBody();
