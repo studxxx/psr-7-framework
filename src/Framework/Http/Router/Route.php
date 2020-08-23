d80bb1b76518be4d1df670dd843679b64cb394c5
studxxx <stud181177@gmail.com>
2020-08-23 12:29:42 +0300

    Refactored Router.php and Route.php

--- src/Framework/Http/Router/Route.php
+++ src/Framework/Http/Router/Route.php
@@ -2,6 +2,10 @@
 
 namespace Framework\Http\Router;
 
+use Framework\Http\Router\Exception\RequestNotMatchedException;
+use Framework\Http\Router\Exception\RouteNotFoundException;
+use Psr\Http\Message\ServerRequestInterface;
+
 class Route
 {
     public string $name;
@@ -17,5 +21,50 @@ class Route
         $this->handler = $handler;
         $this->tokens = $tokens;
         $this->methods = $methods;
+    }
+
+    public function match(ServerRequestInterface $request): ?Result
+    {
+        if ($this->methods && !\in_array($request->getMethod(), $this->methods, true)) {
+            return null;
+        }
+
+        $pattern = \preg_replace_callback('~\{([^\}]+)\}~', function ($matches) {
+            $argument = $matches[1];
+            $replace = $this->tokens[$argument] ?? '[^}]+';
+            return "(?P<$argument>$replace)";
+        }, $this->pattern);
+
+        if (preg_match("~^$pattern$~i", $request->getUri()->getPath(), $matches)) {
+            return new Result(
+                $this->name,
+                $this->handler,
+                array_filter($matches, '\is_string', ARRAY_FILTER_USE_KEY)
+            );
+        }
+        return null;
+    }
+
+    public function generate(string $name, array $params = []): ?string
+    {
+        $arguments = array_filter($params);
+
+        if ($name !== $this->name) {
+            return null;
+        }
+
+        $url = \preg_replace_callback('~\{([^\}]+)\}~', function ($matches) use (&$arguments) {
+            $argument = $matches[1];
+            if (!array_key_exists($argument, $arguments)) {
+                throw new \InvalidArgumentException("Missing parameter \"$argument\"");
+            }
+            return $arguments[$argument];
+        }, $this->pattern);
+
+        if ($url !== null) {
+            return $url;
+        }
+
+        return null;
     }
 }
