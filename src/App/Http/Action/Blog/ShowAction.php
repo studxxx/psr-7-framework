4a36d6244229496f92e37fae19d5bba38b8f9d41
studxxx <stud181177@gmail.com>
2020-08-25 13:06:18 +0300

    Added tests

--- src/App/Http/Action/Blog/ShowAction.php
+++ src/App/Http/Action/Blog/ShowAction.php
@@ -4,6 +4,7 @@ namespace App\Http\Action\Blog;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
 
 class ShowAction
@@ -12,7 +13,7 @@ class ShowAction
     {
         $id = $request->getAttribute('id');
         if ($id > 2) {
-            return new JsonResponse(['error' => 'Undefined page'], 404);
+            return new HtmlResponse('Undefined page', 404);
         }
         return new JsonResponse(['id' => $id, 'title' => "Post #$id"]);
     }
---
+++ tests/App/Http/Action/Blog/ShowActionTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Tests\App\Http\Action\Blog;
+
+use App\Http\Action\Blog\ShowAction;
+use PHPUnit\Framework\TestCase;
+use Zend\Diactoros\ServerRequest;
+
+class ShowActionTest extends TestCase
+{
+    /**
+     * @covers
+     */
+    public function testSuccess(): void
+    {
+        $action = new ShowAction();
+
+        $request = (new ServerRequest())
+            ->withAttribute('id', $id = 2);
+
+        $response = $action($request);
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertJsonStringEqualsJsonString(
+            json_encode(['id' => $id, 'title' => 'Post #' . $id]),
+            $response->getBody()->getContents()
+        );
+    }
+
+    /**
+     * @covers
+     */
+    public function testNotFound(): void
+    {
+        $action = new ShowAction();
+
+        $request = (new ServerRequest())
+            ->withAttribute('id', $id = 10);
+
+        $response = $action($request);
+
+        self::assertEquals(404, $response->getStatusCode());
+        self::assertEquals('Undefined page', $response->getBody()->getContents());
+    }
+}
