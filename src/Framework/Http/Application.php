81ca0eff9d94e8868e4a8d9aa42ecb6afd4b670c
studxxx <stud181177@gmail.com>
2020-08-29 08:12:38 +0300

    Added application

---
+++ src/Framework/Http/Application.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Framework\Http;
+
+use Framework\Http\Pipeline\MiddlewareResolver;
+use Framework\Http\Pipeline\Pipeline;
+
+class Application extends Pipeline
+{
+    private MiddlewareResolver $resolver;
+
+    public function __construct(MiddlewareResolver $resolver)
+    {
+        parent::__construct();
+        $this->resolver = $resolver;
+    }
+
+    public function pipe($middleware): void
+    {
+        parent::pipe($this->resolver->resolve($middleware));
+    }
+}
