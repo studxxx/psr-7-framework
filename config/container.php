fbac22fb073dd33482c0172c8382bab59a97a98e
studxxx <stud181177@gmail.com>
2020-08-30 09:57:27 +0300

    renamed file to dependencies.php

--- config/container.php
+++ config/container.php
@@ -4,6 +4,6 @@ use Framework\Container\Container;
 
 $container = new Container();
 $container->set('config', require 'config/params.php');
-require 'config/definitions.php';
+require 'config/dependencies.php';
 
 return $container;
