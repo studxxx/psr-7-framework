60ee888ac4f5e6f190e59f247cf0c097104a8506
studxxx <stud181177@gmail.com>
2020-09-01 06:06:54 +0300

    Fix render template

--- src/App/Http/Action/HelloAction.php
+++ src/App/Http/Action/HelloAction.php
@@ -22,8 +22,6 @@ class HelloAction
         ob_start();
         require 'templates/' . $view . '.php';
 
-        $html = ob_get_contents();
-        ob_clean();
-        return $html;
+        return ob_get_clean();
     }
 }
