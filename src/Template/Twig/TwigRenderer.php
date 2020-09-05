04724faaa4e37a6e33030e2f0c876c7b465f7aa3
studxxx <stud181177@gmail.com>
2020-09-05 13:22:33 +0300

    Added twig renderer

---
+++ src/Template/Twig/TwigRenderer.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Template\Twig;
+
+use Template\TemplateRenderer;
+use Twig\Environment;
+
+class TwigRenderer implements TemplateRenderer
+{
+    private Environment $twig;
+    private string $extension;
+
+    public function __construct(Environment $twig, string $extension)
+    {
+        $this->twig = $twig;
+        $this->extension = $extension;
+    }
+
+    public function render($name, array $params = []): string
+    {
+        return $this->twig->render($name . $this->extension, $params);
+    }
+}
