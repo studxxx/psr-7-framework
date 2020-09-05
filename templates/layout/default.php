82eb09e9a288a81806f965ce606df07d83fe7706
studxxx <stud181177@gmail.com>
2020-09-05 08:28:46 +0300

    Fix template

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -29,7 +29,7 @@
     </style>
 </head>
 
-<body>
+<body class="app">
 
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <a class="navbar-brand" href="<?= $this->encode($this->path('home')) ?>">Application</a>
