d91c8862c799cff034f32f5202cec97b09183bd3
studxxx <stud181177@gmail.com>
2020-09-12 12:43:01 +0300

    The diactoros package is decoupled from the generator

--- src/App/Http/Middleware/ErrorHandler/HtmlErrorResponseGenerator.php
+++ src/App/Http/Middleware/ErrorHandler/HtmlErrorResponseGenerator.php
@@ -5,29 +5,34 @@ namespace App\Http\Middleware\ErrorHandler;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Template\TemplateRenderer;
-use Zend\Diactoros\Response;
-use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Stratigility\Utils;
 
 class HtmlErrorResponseGenerator implements ErrorResponseGenerator
 {
     private TemplateRenderer $template;
     private array $views;
+    private ResponseInterface $response;
 
-    public function __construct(TemplateRenderer $template, array $views)
+    public function __construct(TemplateRenderer $template, array $views, ResponseInterface $response)
     {
         $this->template = $template;
         $this->views = $views;
+        $this->response = $response;
     }
 
     public function generate(\Throwable $e, ServerRequestInterface $request): ResponseInterface
     {
-        $code = Utils::getStatusCode($e, new Response());
+        $code = Utils::getStatusCode($e, $this->response);
 
-        return new HtmlResponse($this->template->render($this->getView($code), [
-            'request' => $request,
-            'exception' => $e,
-        ]), $code);
+        $response = $this->response->withStatus($code);
+        $response
+            ->getBody()
+            ->write($this->template->render($this->getView($code), [
+                'request' => $request,
+                'exception' => $e,
+            ]));
+
+        return $response;
     }
 
     private function getView(int $code): string
