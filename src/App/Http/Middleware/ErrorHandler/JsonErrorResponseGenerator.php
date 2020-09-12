4584d41cc7d56a7884421d1cfd125c696a3fb066
studxxx <stud181177@gmail.com>
2020-09-12 10:53:19 +0300

    Added JsonErrorResponseGenerator.php

---
+++ src/App/Http/Middleware/ErrorHandler/JsonErrorResponseGenerator.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Middleware\ErrorHandler;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\Response\JsonResponse;
+use Zend\Stratigility\Utils;
+
+class JsonErrorResponseGenerator implements ErrorResponseGenerator
+{
+    public function generate(\Throwable $e, ServerRequestInterface $request): ResponseInterface
+    {
+        return new JsonResponse([
+            'error' => $e->getMessage(),
+        ], Utils::getStatusCode($e, new Response()));
+    }
+}
