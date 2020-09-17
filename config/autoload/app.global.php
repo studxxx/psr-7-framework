964056e0a8854adc2df0e7c45bc0b42f041385ef
studxxx <stud181177@gmail.com>
2020-09-17 09:19:23 +0300

    Separate config for dev and prod

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -5,19 +5,16 @@ use Aura\Router\RouterContainer;
 use Framework\Http\Application;
 use Framework\Http\Middleware\ErrorHandler\ErrorHandlerMiddleware;
 use Framework\Http\Middleware\ErrorHandler\ErrorResponseGenerator;
-use Framework\Http\Middleware\ErrorHandler\WhoopsErrorResponseGenerator;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
 use Infrastructure\Framework\Http\Middleware\ErrorHandler\HtmlErrorResponseGenerator;
 use Infrastructure\Framework\Http\Middleware\ErrorHandler\LogErrorListener;
+use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Template\TemplateRenderer;
-use Whoops\Handler\PrettyPageHandler;
-use Whoops\Run;
-use Whoops\RunInterface;
 use Zend\Diactoros\Response;
 use Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory;
 
@@ -31,8 +28,7 @@ return [
                 return new Application(
                     $container->get(MiddlewareResolver::class),
                     $container->get(Router::class),
-                    $container->get(Middleware\NotFoundHandler::class),
-                    new Response()
+                    $container->get(Middleware\NotFoundHandler::class)
                 );
             },
             Router::class => function () {
@@ -49,12 +45,6 @@ return [
                 return $middleware;
             },
             ErrorResponseGenerator::class => function (ContainerInterface $container) {
-                if ($container->get('config')['debug']) {
-                    return new WhoopsErrorResponseGenerator(
-                        $container->get(RunInterface::class),
-                        new Response()
-                    );
-                }
                 return new HtmlErrorResponseGenerator(
                     $container->get(TemplateRenderer::class),
                     new Response(),
@@ -65,18 +55,10 @@ return [
                     ]
                 );
             },
-            RunInterface::class => function () {
-                $whoops = new Run();
-                $whoops->writeToOutput(false);
-                $whoops->allowQuit(false);
-                $whoops->pushHandler(new PrettyPageHandler());
-                $whoops->register();
-                return $whoops;
-            },
 
             LoggerInterface::class => function (ContainerInterface $container) {
                 $logger = new Logger('App');
-                $logger->pushHandler(new \Monolog\Handler\StreamHandler(
+                $logger->pushHandler(new StreamHandler(
                     'var/log/application.log',
                     $container->get('config')['debug'] ? Logger::DEBUG : Logger::WARNING
                 ));
