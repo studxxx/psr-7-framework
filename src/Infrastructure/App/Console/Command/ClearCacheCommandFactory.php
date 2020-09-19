876dbda170684733fd7c2b72fdec80466f1a8a1f
studxxx <stud181177@gmail.com>
2020-09-19 12:04:42 +0300

    Updated ClearCacheCommand.php

--- src/Infrastructure/App/Console/Command/ClearCacheCommandFactory.php
+++ src/Infrastructure/App/Console/Command/ClearCacheCommandFactory.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace Infrastructure\App\Console\Command;
 
 use App\Console\Command\ClearCacheCommand;
+use App\Service\FileManager;
 use Psr\Container\ContainerInterface;
 
 class ClearCacheCommandFactory
 {
     public function __invoke(ContainerInterface $container): ClearCacheCommand
     {
-        return new ClearCacheCommand($container->get('config')['console']['cachePaths']);
+        return new ClearCacheCommand(
+            $container->get('config')['console']['cachePaths'],
+            $container->get(FileManager::class)
+        );
     }
 }
