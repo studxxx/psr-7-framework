1e67aaa206e73c1e621f78621d765a7972b359bb
studxxx <stud181177@gmail.com>
2020-09-19 07:38:52 +0300

    Moved clear cache command to ClearCacheCommand.php class

--- bin/app.php
+++ bin/app.php
@@ -7,31 +7,8 @@ require 'vendor/autoload.php';
 /** @var \Psr\Container\ContainerInterface $container */
 $container = require 'config/container.php';
 
-function delete ($path) {
-    if (!file_exists($path)) {
-        return true;
-    }
-
-    if (!is_dir($path)) {
-        return unlink($path);
-    }
-
-    foreach (scandir($path, SCANDIR_SORT_ASCENDING) as $item) {
-        if ($item === '.' || $item === '..') {
-            continue;
-        }
-
-        if (!delete($path . DIRECTORY_SEPARATOR . $item)) {
-            return false;
-        }
-    }
-
-    return rmdir($path);
-}
-
 echo 'Clearing cache' . PHP_EOL;
-delete('var/cache/twig');
-/** @var \Psr\Log\LoggerInterface $logger */
-$logger = $container->get(\Psr\Log\LoggerInterface::class);
-$logger->info('Clearing cache');
-echo 'Done!' . PHP_EOL;
+
+$command = $container->get(\App\Console\Command\ClearCacheCommand::class);
+
+$command->execute();
