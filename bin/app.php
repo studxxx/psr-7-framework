2deffc7b698f004b22db2da1d1e1884b5c8c6671
studxxx <stud181177@gmail.com>
2020-09-19 13:56:52 +0300

    Added console Application.php class

--- bin/app.php
+++ bin/app.php
@@ -10,27 +10,6 @@ require 'vendor/autoload.php';
 /** @var \Psr\Container\ContainerInterface $container */
 $container = require 'config/container.php';
 
-/** @var \Framework\Console\Command[] $commands */
-$commands = [
-    $container->get(ClearCacheCommand::class),
-];
-
-$input = new Console\Input($argv);
-$output = new Console\Output();
-$name = $input->getArgument(0);
-
-if (!empty($name)) {
-    foreach ($commands as $command) {
-        if ($command->getName() === $name) {
-            $command->execute($input, $output);
-            exit;
-        }
-    }
-    throw new InvalidArgumentException("Undefined command $name");
-}
-
-$output->writeln('<comment>Available commands:</comment>');
-$output->writeln('');
-foreach ($commands as $command) {
-    $output->writeln("<info>{$command->getName()}</info>\t{$command->getDescription()}");
-}
+$cli = new Console\Application();
+$cli->add($container->get(ClearCacheCommand::class));
+$cli->run(new Console\Input($argv), new Console\Output());
