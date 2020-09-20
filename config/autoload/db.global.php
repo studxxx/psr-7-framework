541e2615389f15796db59b06e2743c550854586c
studxxx <stud181177@gmail.com>
2020-09-20 11:08:45 +0300

    Moved connection to DB into factory

---
+++ config/autoload/db.global.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Infrastructure\App\PDOFactory;
+
+return [
+    'dependencies' => [
+        'factories' => [
+            PDO::class => PDOFactory::class,
+        ],
+    ],
+    'pdo' => [
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ]
+];
