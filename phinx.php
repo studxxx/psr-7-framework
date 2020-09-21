48f55062a37397fea6b747482908b159bfc1e4cf
studxxx <stud181177@gmail.com>
2020-09-21 08:43:43 +0300

    Added phinx config

---
+++ phinx.php
@@ -0,0 +1,41 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'production' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'psr7',
+            'user' => 'root',
+            'pass' => 'secret',
+            'port' => '33306',
+            'charset' => 'utf8',
+        ],
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'psr7',
+            'user' => 'root',
+            'pass' => 'secret',
+            'port' => '33306',
+            'charset' => 'utf8',
+        ],
+        'testing' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'psr7_test',
+            'user' => 'root',
+            'pass' => 'secret',
+            'port' => '33306',
+            'charset' => 'utf8',
+        ]
+    ],
+    'version_order' => 'creation'
+];
