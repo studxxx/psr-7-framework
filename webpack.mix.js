75ef0fd281d0c1266d2d59cec9c3649821c7d7de
studxxx <stud181177@gmail.com>
2020-10-04 07:06:48 +0300

    added Laravel's webpack.mix.js

---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+let mix = require('laravel-mix');
+
+mix
+    .setPublicPath('public/build')
+    .setResourceRoot('/build/')
+    .js('resources/assets/js/app.js', 'js')
+    .sass('resources/assets/sass/app.scss', 'css')
+    .version();
